e58cda8203e702446a90d6e938a5c82da2b4aee2
paul <paul.18.sma@gmail.com>
2020-05-26 20:57:01 -0300

    adds home component and it route

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// guards
+import { AuthGuard } from "@auth/guards/auth.guard";
+// compoentns
+import { DashboardComponent } from '@dashboard/dashboard.component';
+import { HomeComponent } from '@dashboard/components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
