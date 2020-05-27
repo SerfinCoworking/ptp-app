4e794ef2715c8fc7c0d7f149e58215830e4ba33a
paul <paul.18.sma@gmail.com>
2020-05-26 22:14:01 -0300

    adds sidebar import, and wrapper styles

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule, routingComponents } from './dashboard-routing.module';
+// components without routes
+import { SidebarComponent } from '@dashboard/components/layouts/sidebar/sidebar.component';
+
+// material modules
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @NgModule({
-  declarations: [routingComponents],
+  declarations: [
+    routingComponents,
+    SidebarComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatIconModule
   ]
 })
 export class DashboardModule { }
