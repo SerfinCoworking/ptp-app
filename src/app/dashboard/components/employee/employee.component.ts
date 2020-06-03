3fb5ea4c217f1820be8030643f7ada4ad2c8653c
paul <paul.18.sma@gmail.com>
2020-06-03 20:49:54 -0300

    adds employee submenu component

---
+++ src/app/dashboard/components/employee/employee.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-table-header-component',
+  templateUrl: './employee.component.html'
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
