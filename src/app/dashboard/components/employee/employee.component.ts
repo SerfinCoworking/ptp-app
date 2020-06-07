03630c85d5fa792aa72b0da08affc03841b302a7
paul <paul.18.sma@gmail.com>
2020-06-07 02:22:40 -0300

    change employee template: adds list and form in it, and show form accordding with the route path

--- src/app/dashboard/components/employee/employee.component.ts
+++ src/app/dashboard/components/employee/employee.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'employee-submenu',
   templateUrl: './employee.component.html'
 })
-export class EmployeeComponent implements OnInit {
+export class EmployeeComponent implements OnInit, OnDestroy{
 
-  constructor() { }
+  private subscription: Subscription = new Subscription();
+  showForm: boolean = false;
+  constructor(private route: ActivatedRoute,) { }
 
   ngOnInit(): void {
+    this.subscription.add(this.route.data.subscribe(data => {
+      this.showForm = typeof(data.form) === 'boolean' ? data.form : false;
+    }));
   }
 
+  ngOnDestroy(): void{
+    this.subscription.unsubscribe();
+  }
 }
