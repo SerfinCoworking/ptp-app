11839432da32c7277d17f52b09a19e4e29c9c5ac
paul <paul.18.sma@gmail.com>
2020-06-07 02:20:34 -0300

    removes card layout, and adds unsubscribe on destroy component

--- src/app/dashboard/components/employee/employee-list/employee-list.component.ts
+++ src/app/dashboard/components/employee/employee-list/employee-list.component.ts
@@ -1,40 +1,38 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { EmployeeService } from '@dashboard/services/employee.service';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-
+import { IEmployee } from '@interfaces/employee';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.sass']
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeListComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription = new Subscription();
   displayedColumns: string[] = ['fullName', 'dni', 'email', 'phone'];
-  dataSource: MatTableDataSource<any>;
+  dataSource: MatTableDataSource<IEmployee[]>;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  constructor(private employeeServie: EmployeeService) { }
+  constructor(private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
 
-    this.employeeServie.getEmployees().subscribe(
+    this.subscription.add(this.employeeService.employees.subscribe(
       employees => {
         this.dataSource = new MatTableDataSource<any>(employees);
         this.dataSource.paginator = this.paginator;
       }
-    )
+    ));
   }
 
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
 
 
