dd7fb63e7f6e38e1f86339fba78990e8b73489a9
paul <paul.18.sma@gmail.com>
2020-06-07 02:27:33 -0300

    adds addEmployee method and employees observable

--- src/app/dashboard/services/employee.service.ts
+++ src/app/dashboard/services/employee.service.ts
@@ -1,16 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@root/environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs';
+import { IEmployee } from '@interfaces/employee';
+import { mapTo, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private subscription: Subscription = new Subscription();
+  private _employees: BehaviorSubject<IEmployee[]> = new BehaviorSubject<IEmployee[]>([]);
 
   constructor(private http: HttpClient) { }
 
-  getEmployees(): Observable<any>{
-    return this.http.get<any>(`${environment.API_END_POINT}/employees`);
+  getEmployees(): Observable<boolean>{
+    return this.http.get<IEmployee[]>(`${environment.API_END_POINT}/employees`).pipe(
+      tap((employees: IEmployee[]) => this.setEmployees(employees)),
+      mapTo(true)
+    );
+  }
+
+  addEmployee(employee: IEmployee): Observable<boolean>{
+    return this.http.post<IEmployee>(`${environment.API_END_POINT}/employees`, employee).pipe(
+      tap(employee => this.pushEmployee(employee)),
+      mapTo(true)
+    );
+  }
+
+  private pushEmployee(employee: IEmployee){
+    this._employees.next([...this.getEmployeesAsArray(), ...[employee]]);
+  }
+
+  private getEmployeesAsArray(): IEmployee[]{
+    let employees: IEmployee[];
+    this.subscription.add(this.employees.subscribe( employeesResult => employees = employeesResult));
+    this.subscription.unsubscribe();
+    return employees;
+  }
+
+  setEmployees(employees: IEmployee[]){
+    this._employees.next(employees);
+  }
+
+  get employees(): Observable<IEmployee[]>{
+    return this._employees.asObservable();
   }
 }
