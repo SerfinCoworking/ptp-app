0d581609682e6c8f31df537ffd48ad67e637b118
paul <paul.18.sma@gmail.com>
2020-06-17 23:04:49 -0300

    adds 'empleados' and 'crear' as link

--- src/app/dashboard/components/employee/employee-header.component.ts
+++ src/app/dashboard/components/employee/employee-header.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription, Observable } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
-import { EmployeeService } from '@dashboard/services/employee.service';
 
 @Component({
   selector: 'employee-submenu',
@@ -9,27 +6,10 @@ import { EmployeeService } from '@dashboard/services/employee.service';
 })
 export class EmployeeHeaderComponent implements OnInit, OnDestroy {
 
-  private subscription: Subscription = new Subscription();
+  constructor() {}
 
+  ngOnInit(): void {}
 
-  constructor(private employeeService: EmployeeService) { }
-
-  ngOnInit(): void {
-    this.subscription.add(
-      this.employeeService.showForm
-    );
-  }
-
-  openForm(): void{
-    this.employeeService.showForm();
-  }
-
-  closeForm(): void{
-    this.employeeService.hideForm();
-  }
-
-  ngOnDestroy(): void{
-    this.subscription.unsubscribe();
-  }
+  ngOnDestroy(): void{}
 
 }
