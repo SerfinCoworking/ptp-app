0123d4879e115684c66c9f2dac3a6ff48a3540a1
paul <paul.18.sma@gmail.com>
2020-11-03 18:24:09 -0300

    adds cursor pointer on avatar

--- src/app/dashboard/modules/liquidation/components/form/form.component.ts
+++ src/app/dashboard/modules/liquidation/components/form/form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IPeriod } from '@interfaces/schedule';
+import {NgbDate} from '@ng-bootstrap/ng-bootstrap';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+
+  hoveredDate: NgbDate | null = null;
+
+  initCalendar: {year: number, month: number};
+  rangeFromDate: NgbDate;
+  rangeToDate: NgbDate | null = null;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initCalendar = {year: moment().year(), month: parseInt(moment().format("M"))}; 
   }
 
+  // period selection
+  onDateSelection(date: NgbDate) {
+    if (!this.rangeFromDate && !this.rangeToDate) {
+      this.rangeFromDate = date;
+    } else if (this.rangeFromDate && !this.rangeToDate && date.after(this.rangeFromDate)) {
+      this.rangeToDate = date;
+    } else {
+      this.rangeToDate = null;
+      this.rangeFromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return this.rangeFromDate && !this.rangeToDate && this.hoveredDate && date.after(this.rangeFromDate) && date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate) {
+    return this.rangeToDate && date.after(this.rangeFromDate) && date.before(this.rangeToDate);
+  }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.rangeFromDate) || (this.rangeToDate && date.equals(this.rangeToDate)) || this.isInside(date) || this.isHovered(date);
+  }
+
+  buildLiquidationSchema(){
+    
+  }
 }
