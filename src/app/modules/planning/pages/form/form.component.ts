2e3eaf32b6a8967cae51ffcfeeb2ddc01fe31427
paul <paul.18.sma@gmail.com>
2021-09-23 00:12:40 -0300

    improve schedule planning slider by weeks

--- src/app/modules/planning/pages/form/form.component.ts
+++ src/app/modules/planning/pages/form/form.component.ts
@@ -11,30 +11,34 @@ export class FormComponent implements OnInit {
 
   xAxis: string = '0';
   xAxisPage: number = 0;
+  maxHours: number = 48;
   planning: any;
+  weeksHeader: any;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     
     this.activatedRoute.data.subscribe( data => {
-      console.log(data, "<================");
-      this.planning = data.planning;
+      this.planning = data.planning.weeksEvents;
+      console.log(this.planning, "<================");
+      this.weeksHeader = data.planning.weeks;
+      console.log(this.weeksHeader, "<================");
     });
   }
 
-  // prevWeek(){
-  //   if(this.xAxisPage > 0){
-  //     this.xAxisPage--
-  //     this.xAxis = (this.xAxisPage * -100) + '%';
-  //   }
-  // }
+  prevWeek(){
+    if(this.xAxisPage > 0){
+      this.xAxisPage--
+      this.xAxis = (this.xAxisPage * -100) + '%';
+    }
+  }
 
-  // nextWeek(){
-  //   if(this.xAxisPage < (this.periodBuilder.length - 1)){
-  //     this.xAxisPage++
-  //     this.xAxis = (this.xAxisPage * -100) + '%';
-  //   }
-  // }
+  nextWeek(){
+    if(this.xAxisPage < (this.weeksHeader.length - 1)){
+      this.xAxisPage++
+      this.xAxis = (this.xAxisPage * -100) + '%';
+    }
+  }
 
 }
