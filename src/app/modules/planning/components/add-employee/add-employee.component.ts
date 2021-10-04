14d144f5922fc9709c6748f787563a668f7de086
paul <paul.18.sma@gmail.com>
2021-10-03 23:41:30 -0300

    adds search employee with other events

--- src/app/modules/planning/components/add-employee/add-employee.component.ts
+++ src/app/modules/planning/components/add-employee/add-employee.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { IEmployee } from '@shared/models/employee';
+import { IPeriod } from '@shared/models/schedule';
+import { PeriodService } from '@shared/services/period.service';
 
 @Component({
   selector: 'app-add-employee',
@@ -9,19 +11,22 @@ import { IEmployee } from '@shared/models/employee';
 })
 export class AddEmployeeComponent implements OnInit {
 
+  @Input() period: IPeriod;
+
   findEmployeeForm: FormGroup = this.fBuild.group({
     employee: ['']
   });
 
   filteredEmployees: Array<IEmployee> = [];
 
-  constructor(private fBuild: FormBuilder) { }
+  constructor(private fBuild: FormBuilder, private periodService: PeriodService) { }
 
   ngOnInit(): void {
     this.findEmployeeForm.valueChanges.subscribe((form) => {
       if(form.employee.length > 3){
-        console.log(form);
-
+        this.periodService.getEmployeesForPlanning(this.period.fromDate, this.period.toDate, form.employee).subscribe((res) => {
+          console.log(res);
+        });
       }
     });
   }
--- src/app/shared/services/period.service.ts
+++ src/app/shared/services/period.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@root/environments/environment';
 import { IPeriod } from '@shared/models/schedule';
@@ -17,5 +17,14 @@ export class PeriodService {
   
   periodPlanning(id: string): Observable<any>{
     return this.http.get<any>(`${environment.API_END_POINT}/period/${id}/planning`);
+  }
+  
+  getEmployeesForPlanning(fromDate: string, toDate: string, employee: string): Observable<any>{
+    let params = new HttpParams();
+    params = params.append('fromDate', fromDate);
+    params = params.append('toDate', toDate);
+    params = params.append('employee', employee);
+
+    return this.http.get<any>(`${environment.API_END_POINT}/period/employees-for-planning`, {params});
   }
 }
