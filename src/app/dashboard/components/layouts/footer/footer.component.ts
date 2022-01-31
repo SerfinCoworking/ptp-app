f82604e252b32d6a83a8d78d81d53bfb64f68776
paul <paul.18.sma@gmail.com>
2022-01-30 21:08:52 -0300

    improve copyright year

--- src/app/dashboard/components/layouts/footer/footer.component.ts
+++ src/app/dashboard/components/layouts/footer/footer.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import moment from 'moment';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.sass']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  copyrightYear = '';
+  constructor() { 
+    this.copyrightYear = moment().format("YYYY");
   }
-
 }
